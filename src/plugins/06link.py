6f9197ed9e6533eba113af01837195bf3ed31ca5
Álvaro Torralba <donfrutosgomez@gmail.com>
2019-04-06 15:45:30 -0300

    Actualizado el plug-in 06link.py
    Se modificó y renombró el archivo 05link.py a 06lik.py.

--- src/plugins/06link.py
+++ src/plugins/06link.py
@@ -6,4 +6,4 @@ web_label = markup.label_start_markup + "info" + markup.label_end_markup
 web_link = markup.text_start_markup + "<a href='http://huayra.conectarigualdad.gob.ar/'>sitio web</a>" + markup.text_end_markup
 
 
-info_table.add_row_to_table(web_label, web_link, 6, destino="gui")
+info_table.add_row_to_table(web_label, web_link, 8, destino="gui")
